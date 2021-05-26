c9fc0363c44d4f69037b37e9e3e6d9cc60e260d7
Arie <arie.oldman@vhs7.tv>
2021-05-26 11:48:25 +1000

    moves Timesheet into resource file

---
+++ pkg/deputy/resource.go
@@ -0,0 +1,47 @@
+package deputy
+
+type Timesheet struct {
+	Id                  int
+	Employee            int
+	EmployeeHistory     int
+	EmployeeAgreement   int
+	Date                string
+	StartTime           int
+	EndTime             int
+	Mealbreak           string
+	MealbreakSlots      string
+	TotalTime           float64
+	TotalTimeInv        float64
+	Cost                float64
+	Roster              int
+	EmployeeComment     string
+	SupervisorComment   string
+	Supervisor          int
+	Disputed            bool
+	TimeApproved        bool
+	TimeApprover        int
+	Discarded           bool
+	ValidationFlag      int
+	OperationalUnit     int
+	IsInProgress        bool
+	IsLeave             bool
+	LeaveId             int
+	LeaveRule           int
+	Invoiced            bool
+	InvoiceComment      string
+	PayRuleApproved     bool
+	Exported            bool
+	StagingId           int
+	PayStaged           bool
+	PaycycleId          int
+	File                int
+	CustomFieldData     int
+	RealTime            bool
+	AutoProcessed       bool
+	AutoRounded         bool
+	AutoTimeApproved    bool
+	AutoPayRuleApproved bool
+	Creator             int
+	Created             string
+	Modified            string
+}
