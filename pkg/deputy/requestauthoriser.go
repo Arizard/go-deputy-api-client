3997fcd6558efc41766286e81327a89b604e8f58
Arie <arie.oldman@vhs7.tv>
2021-05-29 17:03:55 +1000

    add doc comment

--- pkg/deputy/requestauthoriser.go
+++ pkg/deputy/requestauthoriser.go
@@ -9,6 +9,8 @@ import (
 // For example, this could add the Authorization header.
 type RequestAuthoriser func(r *http.Request)
 
+// NewBearerTokenRequestAuthoriser returns a RequestAuthoriser which adds
+// bearer token authorisation on to a request.
 func NewBearerTokenRequestAuthoriser(token string) RequestAuthoriser {
 	return func(req *http.Request) {
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
