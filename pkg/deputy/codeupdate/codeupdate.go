2011135c13a9795f2226219f440545fdd2c4d603
Arie <arie.oldman@vhs7.tv>
2021-05-30 09:16:20 +1000

    add more types

--- pkg/deputy/codeupdate/codeupdate.go
+++ pkg/deputy/codeupdate/codeupdate.go
@@ -1,11 +1,11 @@
 package codeupdate
 
 type ScriptOptions struct {
-	DecafBase64 string `json:"strDecaf,omitempty"`
-	DeXMLBase64 string `json:"strDexml,omitempty"`
-	Label       string `json:"strLabel,omitempty"`
-	ScriptType  uint   `json:"intScriptType"`
-	TriggerORM  uint   `json:"strTriggerOrm,omitempty"`
+	DecafBase64 string     `json:"strDecaf,omitempty"`
+	DeXMLBase64 string     `json:"strDexml,omitempty"`
+	Label       string     `json:"strLabel,omitempty"`
+	ScriptType  ScriptType `json:"intScriptType"`
+	TriggerORM  TriggerORM `json:"strTriggerOrm,omitempty"`
 }
 
 type ReportOptions struct {
@@ -31,19 +31,82 @@ type CustomAppOptions struct {
 	CSSBase64        string `json:"strCss"`
 }
 
+type ScriptType uint
+type TriggerORM string
+
 const (
-	HelperSupportScript  uint = 1  // 1 - Helper Support Script
-	ExecuteOnLogin            = 2  // 2 - Execute on Login
-	ExecuteAtCron             = 3  // 3 - Execute at Cron
-	RecordValidation          = 4  // 4 - Record Validation*
-	PeriodPayCalculation      = 6  // 6 - Period Pay Calculation
-	RecordAfterSave           = 7  // 7 - Record After Save*
-	ExecuteViaApi             = 8  // 8 - Executable via SOAP/REST api
-	PayrollExport             = 9  // 9 - Payroll Export Script
-	ApplicationLaunch         = 10 // 10 - Application Launch*
-	ShiftPayCalculation       = 11 // 11 - Shift Pay Calculation
-	KioskLaunch               = 12 // 12 - Kiosk Launch
-	Notification              = 13 // 13 - Notification
-	Widget                    = 14 // 14 - Widget*
-	RecordAfterDelete         = 15 // 15 - Record After Delete*
+	HelperSupportScript  ScriptType = 1  // 1 - Helper Support Script
+	ExecuteOnLogin                  = 2  // 2 - Execute on Login
+	ExecuteAtCron                   = 3  // 3 - Execute at Cron
+	RecordValidation                = 4  // 4 - Record Validation*
+	PeriodPayCalculation            = 6  // 6 - Period Pay Calculation
+	RecordAfterSave                 = 7  // 7 - Record After Save*
+	ExecuteViaApi                   = 8  // 8 - Executable via SOAP/REST api
+	PayrollExport                   = 9  // 9 - Payroll Export Script
+	ApplicationLaunch               = 10 // 10 - Application Launch*
+	ShiftPayCalculation             = 11 // 11 - Shift Pay Calculation
+	KioskLaunch                     = 12 // 12 - Kiosk Launch
+	Notification                    = 13 // 13 - Notification
+	Widget                          = 14 // 14 - Widget*
+	RecordAfterDelete               = 15 // 15 - Record After Delete*
+)
+
+const (
+	TriggerCategory                     TriggerORM = "DeputecCategory"
+	TriggerAddress                                 = "DeputecAddress"
+	TriggerCompany                                 = "DeputecCompany"
+	TriggerCompanyPeriod                           = "DeputecCompanyPeriod"
+	TriggerContact                                 = "DeputecContact"
+	TriggerCountry                                 = "DeputecCountry"
+	TriggerCustomAppData                           = "DeputecCustomAppData"
+	TriggerCustomField                             = "DeputecCustomField"
+	TriggerCustomFieldDataComment                  = "DeputecCustomFieldDataComment"
+	TriggerEmployee                                = "DeputecEmployee"
+	TriggerEmployeeAgreement                       = "DeputecEmployeeAgreement"
+	TriggerEmployeeAgreementHistory                = "DeputecEmployeeAgreementHistory"
+	TriggerEmployeeAppraisal                       = "DeputecEmployeeAppraisal"
+	TriggerEmployeeAvailability                    = "DeputecEmployeeAvailability"
+	TriggerEmployeeHistory                         = "DeputecEmployeeHistory"
+	TriggerEmployeePaycycle                        = "DeputecEmployeePaycycle"
+	TriggerEmployeePaycycleReturn                  = "DeputecEmployeePaycycleReturn"
+	TriggerEmployeeRole                            = "DeputecEmployeeRole"
+	TriggerEmployeeSalaryOpunitCosting             = "DeputecEmployeeSalaryOpunitCosting"
+	TriggerEmployeeWorkplace                       = "DeputecEmployeeWorkplace"
+	TriggerEmploymentCondition                     = "DeputecEmploymentCondition"
+	TriggerEmploymentContract                      = "DeputecEmploymentContract"
+	TriggerEmploymentContractLeaveRules            = "DeputecEmploymentContractLeaveRules"
+	TriggerEvent                                   = "DeputecEvent"
+	TriggerGeo                                     = "DeputecGeo"
+	TriggerJournal                                 = "DeputecJournal"
+	TriggerKiosk                                   = "DeputecKiosk"
+	TriggerLeave                                   = "DeputecLeave"
+	TriggerLeavePayLine                            = "DeputecLeavePayLine"
+	TriggerLeaveAccrual                            = "DeputecLeaveAccrual"
+	TriggerLeaveRules                              = "DeputecLeaveRules"
+	TriggerMemo                                    = "DeputecMemo"
+	TriggerOperationalUnit                         = "DeputecOperationalUnit"
+	TriggerPayRules                                = "DeputecPayRules"
+	TriggerPayPeriod                               = "DeputecPayPeriod"
+	TriggerPublicHoliday                           = "DeputecPublicHoliday"
+	TriggerRoster                                  = "DeputecRoster"
+	TriggerRosterOpen                              = "DeputecRosterOpen"
+	TriggerRosterSwap                              = "DeputecRosterSwap"
+	TriggerRosterTemplate                          = "DeputecRosterTemplate"
+	TriggerSalesData                               = "DeputecSalesData"
+	TriggerSchedule                                = "DeputecSchedule"
+	TriggerSmsLog                                  = "DeputecSmsLog"
+	TriggerState                                   = "DeputecState"
+	TriggerStressProfile                           = "DeputecStressProfile"
+	TriggerTask                                    = "DeputecTask"
+	TriggerTaskGroup                               = "DeputecTaskGroup"
+	TriggerTaskGroupSetup                          = "DeputecTaskGroupSetup"
+	TriggerTaskOpunitConfig                        = "DeputecTaskOpunitConfig"
+	TriggerTaskSetup                               = "DeputecTaskSetup"
+	TriggerTimesheet                               = "DeputecTimesheet"
+	TriggerTimesheetPayReturn                      = "DeputecTimesheetPayReturn"
+	TriggerTeam                                    = "DeputecTeam"
+	TriggerTrainingModule                          = "DeputecTrainingModule"
+	TriggerTrainingRecord                          = "DeputecTrainingRecord"
+	TriggerWebhook                                 = "DeputecWebhook"
+	TriggerShiftTemplate                           = "DeputecShiftTemplate"
 )
